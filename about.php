de346ca1329c06b7d9cc62034b90a7442917c55e
pranto kumar <prantok104@gmail.com>
2022-02-09 18:11:18 +0600

    Service page and about page

---
+++ about.php
@@ -0,0 +1,76 @@
+<?php include 'header.php';?>
+<!--Breadcrumb area start-->
+<div class="ezze-bread-area-satrt" style="background: url('assets/images/breadcrumb/1.jpg') no-repeat center center/cover">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="ezze-bread-content-area">
+                    <h3>About-US</h3>
+                    <div class="ezze-bread-link-area">
+                        <p><a href="index.php">Home</a> <span>/</span> <span>About-US</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Breadcrumb area end-->
+
+<!-- Ezze co-brand area start -->
+<section class="ezze-cobrand-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="ezze-cobrand-context-area">
+                    <div class="ezze-cobrand-context-title">
+                        <h2>Co-Brands :</h2>
+                    </div>
+                    <div class="ezze-cobrand-context-body">
+                        <a href="javscript:void(0)"><img src="assets/images/cobrands/1.png" alt="cobrands"></a>
+                        <a href="javscript:void(0)"><img src="assets/images/cobrands/2.png" alt="cobrands"></a>
+                        <a href="javscript:void(0)"><img src="assets/images/cobrands/3.png" alt="cobrands"></a>
+                        <a href="javscript:void(0)"><img src="assets/images/cobrands/4.png" alt="cobrands"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Ezze co-brand area end -->
+
+
+<!--About area start-->
+<section class="ezze-about-area-start space">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="ezze-about-area-context">
+                    <span class="title-sub">About us</span>
+                    <h4>We have <span class="text-color"><span class="counter">7 </span>+ years of experience</span> in IT services</h4>
+                    <p>Adipisci at, dignissimos dolore dolorem doloribus hic inventore laudantium natus neque nisi nulla officiis sed voluptatibus! Aperiam architecto eaque impedit nemo soluta.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ipsa laborum obcaecati quam quos sit vero. Amet atque cum deserunt dignissimos, doloremque iusto odit quae rerum suscipit temporibus velit veritatis.</p>
+                    <ul class="ezze-about-lists">
+                        <li data-aos="fade-up" data-aos-delay="50" data-aos-duration="2500" class="active"><i class="fal fa-check"></i> Experienced Professionals</li>
+                        <li data-aos="fade-up" data-aos-delay="70" data-aos-duration="2500"><i class="fal fa-check"></i> Excellent Reputation</li>
+                        <li data-aos="fade-up" data-aos-delay="90" data-aos-duration="2500"><i class="fal fa-check"></i> Trustworthy See Reviews</li>
+                        <li data-aos="fade-up" data-aos-delay="110" data-aos-duration="2500"><i class="fal fa-check"></i> Expert Technical Skills</li>
+                        <li data-aos="fade-up" data-aos-delay="130" data-aos-duration="2500"><i class="fal fa-check"></i> Friendly Service</li>
+                        <li data-aos="fade-up" data-aos-delay="150" data-aos-duration="2500" class="active"><i class="fal fa-check"></i> Affordable Diagnosis</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="ezze-about-area-image">
+                    <div class="ezze-about-images" style="background: url('assets/images/about-us/about-shape.png')">
+                        <img data-aos="fade-down-right" data-aos-delay="150" data-aos-duration="3000" src="assets/images/about-us/about1.jpg" alt="About Image">
+                        <img data-aos="zoom-in" data-aos-delay="250" data-aos-duration="3000" src="assets/images/about-us/about2.jpg" alt="About Image">
+                        <img data-aos="fade-up-right" data-aos-delay="150" data-aos-duration="3000" src="assets/images/about-us/about3.jpg" alt="About Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--About area end-->
+
+<?php include 'footer.php';?>
--- service.php
+++ service.php
@@ -1,5 +1,82 @@
 <?php include 'header.php';?>
 
+    <!--Breadcrumb area start-->
+    <div class="ezze-bread-area-satrt" style="background: url('assets/images/breadcrumb/1.jpg') no-repeat center center/cover">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="ezze-bread-content-area">
+                        <h3>Services Page</h3>
+                        <div class="ezze-bread-link-area">
+                            <p><a href="index.php">Home</a> <span>/</span> <span>Services page</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Breadcrumb area end-->
+
+    <!--Service area start-->
+    <div class="ezze-service-main-area-start space">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="ezze-df-project-title">
+                        <span>our best services</span>
+                        <h1>World best IT-Services <br>we provide</h1>
+                    </div>
+                    <div class="ezze-all-services">
+                        <div class="ezze-single-main-service" >
+                            <i class="fal fa-users-cog"></i>
+                            <a href="#">
+                                <h4>managed IT Services</h4>
+                            </a>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis explicabo fugiat porro quam sapiente?</p>
+                        </div>
+                        <div class="ezze-single-main-service" >
+                            <i class="fal fa-filter"></i>
+                            <a href="#">
+                                <h4>Multi-Functional Services</h4>
+                            </a>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus impedit ipsam nam nobis sapiente! Ad, nulla!</p>
+                        </div>
+                        <div class="ezze-single-main-service" >
+                            <i class="fal fa-shield-check"></i>
+                            <a href="#">
+                                <h4>Cyber Security</h4>
+                            </a>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eius exercitationem labore libero minus quia quisquam?</p>
+                        </div>
+                        <div class="ezze-single-main-service" >
+                            <i class="fab fa-linux"></i>
+                            <a href="#">
+                                <h4>Linux Server</h4>
+                            </a>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque autem deserunt esse id illo porro quidem.</p>
+                        </div>
+                        <div class="ezze-single-main-service" >
+                            <i class="fal fa-headset"></i>
+                            <a href="#">
+                                <h4>IT Consultance</h4>
+                            </a>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque illum minus nobis odio pariatur reprehenderit.</p>
+                        </div>
+                        <div class="ezze-single-main-service" >
+                            <i class="fal fa-cloud-drizzle"></i>
+                            <a href="#">
+                                <h4>Cloud</h4>
+                            </a>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, iste, vero. At fugit harum quae vel?</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Service area end-->
+
     <div class="ezze-services-area-start">
         <!--Service single area start-->
         <div class="ezze-single-service-area">
