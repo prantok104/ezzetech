18dc42aff7a02907d9101ed10c34f07c0eab55e1
pranto kumar <prantok104@gmail.com>
2022-02-10 17:03:41 +0600

    cyber-security page add

---
+++ cyber-security.php
@@ -0,0 +1,135 @@
+<?php include('header.php')?>
+<!--Breadcrumb area start-->
+<div class="ezze-bread-area-satrt" style="background: url('assets/images/breadcrumb/1.jpg') no-repeat center center/cover">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="ezze-bread-content-area">
+                    <h3>Cyber Security</h3>
+                    <div class="ezze-bread-link-area">
+                        <p><a href="index.php">Home</a> <span>/</span> <span>Cyber Security</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Breadcrumb area end-->
+
+<!--Cybe security area start-->
+    <div class="ezze-cyber-area-start space">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="ezze-df-project-title">
+                        <span>security must</span>
+                        <h1>World best cyber security <br>we provide 1 month free</h1>
+                    </div>
+                    <div class="ezze-cyber-content-area">
+
+                        <div class="ezze-single-cyber-area">
+                            <div class="plan-container">
+                                <div class="ezze-cyber-plan-head">
+                                    <h4>careBasic</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, natus.</p>
+                                </div>
+                                <div class="ezze-cyber-paln-price">
+                                    <strong>&dollar;29 <span>/month</span></strong>
+                                    <p>Lorem ipsum dolor sit amet.</p>
+                                </div>
+                                <a href="#" class="plan-btn">Start free trial</a>
+                                <p class="cyber-security-list-title">All careBasic services include</p>
+                                <ul>
+                                    <li>24/7 system monitoring</li>
+                                    <li>security management</li>
+                                    <li>Patch management</li>
+                                    <li>remote support</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="ezze-single-cyber-area" aos-init aos-animate" data-aos="zoom-in" data-aos-delay="250" data-aos-duration="1500">
+                            <div class="ezz-cyber-plan-top-bar">Most popular</div>
+                            <div class="plan-container">
+                                <div class="ezze-cyber-plan-head">
+                                    <h4>careBasic</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, natus.</p>
+                                </div>
+                                <div class="ezze-cyber-paln-price">
+                                    <strong>&dollar;29 <span>/month</span></strong>
+                                    <p>Lorem ipsum dolor sit amet.</p>
+                                </div>
+                                <a href="#" class="plan-btn">Start free trial</a>
+                                <p class="cyber-security-list-title">All careBasic services include</p>
+                                <ul>
+                                    <li>24/7 system monitoring</li>
+                                    <li>security management</li>
+                                    <li>Patch management</li>
+                                    <li>remote support</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="ezze-single-cyber-area">
+                           <div class="plan-container">
+                               <div class="ezze-cyber-plan-head">
+                                   <h4>careBasic</h4>
+                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, natus.</p>
+                               </div>
+                               <div class="ezze-cyber-paln-price">
+                                   <strong>&dollar;29 <span>/month</span></strong>
+                                   <p>Lorem ipsum dolor sit amet.</p>
+                               </div>
+                               <a href="#" class="plan-btn">Start free trial</a>
+                               <p class="cyber-security-list-title">All careBasic services include</p>
+                               <ul>
+                                   <li>24/7 system monitoring</li>
+                                   <li>security management</li>
+                                   <li>Patch management</li>
+                                   <li>remote support</li>
+                               </ul>
+                           </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!--Cybe security area end-->
+
+    <!--cyber page parallux area start-->
+        <div class="ezze-cyber-parallax-area-start space" style="background: url('assets/images/service/bg.jpg') no-repeat fixed center center/ cover">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="ezz-parallax-content">
+                            <i class="fal fa-quote-left"></i>
+                            <p>Ezzetech are always accommodating our diverse needs and we feel like they are a part of our company rather than an external supplier.</p>
+                            <span>JHON. json helman.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!--cyber page parallux area end-->
+
+
+    <!-- Contact area start -->
+    <div class="ezze-contact-area-start space-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 m-auto">
+                    <div class="ezze-contact-content text-center">
+                        <span class="title-sub">Let's get started</span>
+                        <h3>Are you ready for a better, more productive business?</h3>
+                        <p>Stop worrying about technology problems. Focus on your business. Let us provide the support you deserve.</p>
+                        <a href="javascript:void(0)">Contact us Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact area end -->
+
+<?php include('footer.php')?>
