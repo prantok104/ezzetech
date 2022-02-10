3f7aadb3e0ce6d410759afdd355ec4662e5b1e43
pranto kumar <prantok104@gmail.com>
2022-02-10 17:02:43 +0600

    Add Managed page

---
+++ managed-service.php
@@ -0,0 +1,138 @@
+<?php include('header.php')?>
+    <!--Breadcrumb area start-->
+    <div class="ezze-bread-area-satrt" style="background: url('assets/images/breadcrumb/1.jpg') no-repeat center center/cover">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="ezze-bread-content-area">
+                        <h3>Managed IT services</h3>
+                        <div class="ezze-bread-link-area">
+                            <p><a href="index.php">Home</a> <span>/</span> <span>Managed IT services</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Breadcrumb area end-->
+
+    <!--Managed IT service area start-->
+    <div class="ezze-managed-area-start space">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="ezze-df-project-title">
+                        <span>overview</span>
+                        <h1>What is managed IT services?</h1>
+                    </div>
+
+                    <div class="ezze-managed-all-services">
+                        <div class="single-managed-service">
+                            <i class="fal fa-crown"></i>
+                            <p><span class="counter">30</span> <span>+</span></p>
+                            <p>Years of experience</p>
+                        </div>
+                        <div class="single-managed-service">
+                            <i class="fal fa-award"></i>
+                            <p><span class="counter">250</span> <span>+</span></p>
+                            <p>Certified experts</p>
+                        </div>
+                        <div class="single-managed-service">
+                            <i class="fal fa-users"></i>
+                            <p><span class="counter">100</span> <span>%</span></p>
+                            <p>End user satisfaction</p>
+                        </div>
+                        <div class="single-managed-service">
+                            <i class="fal fa-globe"></i>
+                            <p><span class="counter">18</span> <span>countries</span></p>
+                            <p>Global reach</p>
+                        </div>
+                        <div class="single-managed-service">
+                            <i class="fal fa-user-headset"></i>
+                            <p><span class="counter">365</span> <span>24/7</span></p>
+                            <p>Live support</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Managed IT service area end-->
+
+    <!--comperhensive area start-->
+    <div class="ezze-com-area-start space">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="ezze-df-project-title">
+                        <span>overview</span>
+                        <h1>What is managed IT services?</h1>
+                    </div>
+
+                    <div class="ezze-com-all-services">
+                        <div class="ezze-single-com-service">
+                            <i class="fal fa-wifi"></i>
+                            <div class="ezze-single-com-content">
+                                <h4>Network</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab fugiat molestias mollitia nobis suscipit vero.</p>
+                            </div>
+                        </div>
+                        <div class="ezze-single-com-service">
+                            <i class="fal fa-user-headset"></i>
+                            <div class="ezze-single-com-content">
+                                <h4>Service desk</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab fugiat molestias mollitia nobis suscipit vero.</p>
+                            </div>
+                        </div>
+                        <div class="ezze-single-com-service">
+                            <i class="fal fa-garage"></i>
+                            <div class="ezze-single-com-content">
+                                <h4>Infrastructure</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab fugiat molestias mollitia nobis suscipit vero.</p>
+                            </div>
+                        </div>
+                        <div class="ezze-single-com-service">
+                            <i class="fal fa-tasks"></i>
+                            <div class="ezze-single-com-content">
+                                <h4>Endpoint management</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab fugiat molestias mollitia nobis suscipit vero.</p>
+                            </div>
+                        </div>
+                        <div class="ezze-single-com-service">
+                            <i class="fal fa-mobile"></i>
+                            <div class="ezze-single-com-content">
+                                <h4>Applications</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab fugiat molestias mollitia nobis suscipit vero.</p>
+                            </div>
+                        </div>
+                        <div class="ezze-single-com-service">
+                            <i class="fal fa-user-plus"></i>
+                            <div class="ezze-single-com-content">
+                                <h4>Managed Services plus</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab fugiat molestias mollitia nobis suscipit vero.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--comperhensive area end-->
+
+    <!-- Contact area start -->
+    <div class="ezze-contact-area-start space-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 m-auto">
+                    <div class="ezze-contact-content text-center">
+                        <span class="title-sub">Let's get started</span>
+                        <h3>Are you ready for a better, more productive business?</h3>
+                        <p>Stop worrying about technology problems. Focus on your business. Let us provide the support you deserve.</p>
+                        <a href="javascript:void(0)">Contact us Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact area end -->
+<?php include('footer.php')?>
