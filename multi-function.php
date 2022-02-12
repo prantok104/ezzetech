b9d80d94092c05dc596347ce9f282d8849d74e2f
Pranto Kumar <40034555+prantok104@users.noreply.github.com>
2022-02-13 00:04:08 +0600

    Multi-function file

---
+++ multi-function.php
@@ -0,0 +1,53 @@
+<?php include('header.php')?>
+<!--Breadcrumb area start-->
+<div class="ezze-bread-area-satrt" style="background: url('assets/images/breadcrumb/1.jpg') no-repeat center center/cover">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="ezze-bread-content-area">
+                    <h3>Multi Function</h3>
+                    <div class="ezze-bread-link-area">
+                        <p><a href="index.php">Home</a> <span>/</span> Services <span>/</span> <span>Multi Function</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Breadcrumb area end-->
+
+<!--choose function area start-->
+<div class="ezze-function-area-start space">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="ezze-function-content-area">
+                    <div class="ezze-function-head">
+                        <h4>Book Appointment</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non impedit dignissimos porro delectus labore quas.</p>
+                    </div>
+                    <form action="#" method="POST">
+                        <div class="ezze-function-form">
+                            <input type="text" name="name" placeholder="Your name">
+                            <input type="email" name="email" placeholder="Your email">
+                            <select name="function" id="function">
+                                <option value="0">choose function</option>
+                                <option value="1">IT consultant</option>
+                                <option value="2">IT services</option>
+                                <option value="3">IT Management</option>
+                                <option value="4">IT security</option>
+                            </select>
+                        </div>
+                        <div class="ezze-function-foot">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, velit asperiores quasi numquam culpa temporibus itaque placeat.</p>
+                            <button type="submit" name="send">Get appointment</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--choose function area end-->
+
+<?php include('footer.php')?>
