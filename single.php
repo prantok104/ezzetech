15382609ccf4fa4c88d8a3e62927cf43fc1ef4f8
pranto kumar <prantok104@gmail.com>
2022-02-13 10:53:01 +0600

    Single page design

---
+++ single.php
@@ -0,0 +1,248 @@
+<?php include 'header.php';?>
+
+<!--Breadcrumb area start-->
+<div class="ezze-bread-area-satrt" style="background: url('assets/images/breadcrumb/1.jpg') no-repeat center center/cover">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="ezze-bread-content-area">
+                    <h3>Details</h3>
+                    <div class="ezze-bread-link-area">
+                        <p><a href="index.php">Home</a> <span>/</span> News <span>/</span> <span>Details</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Breadcrumb area end-->
+
+<!--Single page area start-->
+<div class="ezze-single-page-area-start space">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="ezze-blog-single">
+                    <div class="ezze-news-image-area">
+                        <img src="assets/images/news/1.jpg" alt="news image">
+                    </div>
+                    <div class="ezze-news-content-area">
+                        <div class="ezze-news-info-area">
+                            <p><img src="assets/images/client/1.png" alt="admin-image"> by <a href="#">admin</a> / february 12, 2022</p>
+                        </div>
+                        <a href="single.php" class="news-title"><h2>Improving lives with technology lighthouse project</h2></a>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur delectus nobis veniam. Culpa cupiditate ea illum laudantium pariatur reprehenderit similique tenetur, ut! Beatae cum est ipsam nulla odio vel veritatis ...</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet, delectus distinctio dolore earum eius fugiat laborum minima reprehenderit sapiente sunt, unde ut! Accusamus accusantium aliquam, architecto asperiores aspernatur at, cumque distinctio dolorum earum incidunt laborum maiores nostrum quasi vero.</p>
+                        <blockquote>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cupiditate doloribus, dolorum eius ex impedit nam necessitatibus obcaecati optio pariatur possimus rerum voluptas voluptates?
+                        </blockquote>
+
+                        <ul class="details">
+                            <li>Lorem ipsum dolor sit amet.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, minima!</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam autem eaque inventore ipsa ipsam ipsum nisi perferendis.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, esse excepturi.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+                        </ul>
+                         <h4 class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing.</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum dolore explicabo facilis, magni necessitatibus obcaecati officiis reiciendis rerum suscipit? Animi, debitis distinctio dolor dolorem dolores esse ex hic ipsum iusto laudantium molestiae necessitatibus nisi quo tempore, tenetur veniam vero.</p>
+
+                        <div class="single-page-tags">
+                            <h4>Tags: </h4>
+                            <div class="tags">
+                                <a href="#">IT</a>
+                                <a href="#">IT Services</a>
+                                <a href="#">IT Manager</a>
+                                <a href="#">IT Consultant</a>
+                            </div>
+                        </div>
+
+                        <div class="single-page-tags">
+                            <h4>Share: </h4>
+                            <div class="share">
+                                <a href="#" class="facebook"><i class="fab fa-facebook"></i></a>
+                                <a href="#" class="youtube"><i class="fab fa-youtube"></i></a>
+                                <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
+                                <a href="#" class="instagram"><i class="fab fa-instagram"></i></a>
+                                <a href="#" class="linkedin"><i class="fab fa-linkedin"></i></a>
+                            </div>
+                        </div>
+
+
+                        <!--Comment area start-->
+                        <div class="ezze-comment-area-start">
+                            <h4>3 Replies to "Improving lives with technology lighthouse project"</h4>
+                            <form action="#" method="POST">
+                                <input type="text" name="uname" placeholder="Your name here" class="form-control">
+                                <input type="email" name="email" placeholder="Your email here" class="form-control">
+                                <textarea name="message" id="message" cols="30" rows="5" class="form-control" placeholder="Comment here"></textarea>
+                                <button type="submit" name="comment-btn" class="comment-btn">Comment now</button>
+                            </form>
+
+                            <div class="ezze-all-comments">
+                                <div class="media">
+                                    <div class="media-head">
+                                        <img src="assets/images/client/1.png" class="author-image" alt="author">
+                                    </div>
+                                    <div class="media-body">
+                                        <a href="#">Admin</a>
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                        <span>February 13, 2022</span>
+                                        <a href="#">Replay</a>
+                                        <div class="media">
+                                            <div class="media-head">
+                                                <img src="assets/images/client/2.png" class="author-image" alt="author">
+                                            </div>
+                                            <div class="media-body">
+                                                <a href="#">Nilla</a>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, tenetur.</p>
+                                                <span>February 13, 2022</span>
+                                                <a href="#">Replay</a>
+
+                                                <div class="media">
+                                                    <div class="media-head">
+                                                        <img src="assets/images/client/5.jpg" class="author-image" alt="author">
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <a href="#">Mr. Alex Helx</a>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, tenetur.</p>
+                                                        <span>February 13, 2022</span>
+                                                        <a href="#">Replay</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="media">
+                                    <div class="media-head">
+                                        <img src="assets/images/client/3.png" class="author-image" alt="author">
+                                    </div>
+                                    <div class="media-body">
+                                        <a href="#">Alock Kumar Das</a>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                                        <span>February 13, 2022</span>
+                                        <a href="#">Replay</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!--Comment area end-->
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <aside class="ezze-sidebar-area-start">
+                    <!--single sidebar area start-->
+                    <div class="ezze-single-sidebar">
+                        <div class="ezze-sidebar-header">
+                            <h4>Search</h4>
+                        </div>
+                        <div class="ezze-sidebar-body">
+                            <form action="#" method="POST">
+                                <input type="text" name="search" id="search" placeholder="Type here">
+                                <button type="submit" name="search-btn" id="search-icon"><i class="fal fa-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                    <!--single sidebar area end-->
+                    <!--single sidebar area start-->
+                    <div class="ezze-single-sidebar">
+                        <div class="ezze-sidebar-header">
+                            <h4>Categories</h4>
+                        </div>
+                        <div class="ezze-sidebar-body">
+                            <ul class="category-widget">
+                                <li><a href="blog.php"><i class="fal fa-angle-right"></i> Blog</a></li>
+                                <li><a href="single.php"><i class="fal fa-angle-right"></i> Details</a></li>
+                                <li><a href="news-one.php"><i class="fal fa-angle-right"></i> Style 1</a></li>
+                                <li><a href="news-two.php"><i class="fal fa-angle-right"></i> Style 2</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--single sidebar area end-->
+
+                    <!--single sidebar area start-->
+                    <div class="ezze-single-sidebar">
+                        <div class="ezze-sidebar-header">
+                            <h4>Archive</h4>
+                        </div>
+                        <div class="ezze-sidebar-body">
+                            <ul class="category-widget">
+                                <li><a href="blog.php"><i class="fal fa-angle-right"></i> February 2022</a></li>
+                                <li><a href="blog.php"><i class="fal fa-angle-right"></i> January 2022</a></li>
+                                <li><a href="blog.php"><i class="fal fa-angle-right"></i> December 2021</a></li>
+                                <li><a href="blog.php"><i class="fal fa-angle-right"></i> November 2021</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--single sidebar area end-->
+
+                    <!--single sidebar area start-->
+                    <div class="ezze-single-sidebar">
+                        <div class="ezze-sidebar-header">
+                            <h4>Recent Posts</h4>
+                        </div>
+                        <div class="ezze-sidebar-body">
+
+                            <div class="single-recent-posts">
+                                <img src="assets/images/news/4.jpg" alt="recent post">
+                                <div class="recent-post-content">
+                                    <p>February 12, 2022</p>
+                                    <a href="single.php">5 creative ways to address gaps in It resources and talent</a>
+                                </div>
+                            </div>
+                            <div class="single-recent-posts">
+                                <img src="assets/images/news/3.jpg" alt="recent post">
+                                <div class="recent-post-content">
+                                    <p>February 11, 2022</p>
+                                    <a href="single.php">Improving lives with technology lighthouse project</a>
+                                </div>
+                            </div>
+                            <div class="single-recent-posts">
+                                <img src="assets/images/news/2.jpg" alt="recent post">
+                                <div class="recent-post-content">
+                                    <p>January 10, 2022</p>
+                                    <a href="single.php">Scan & Index manager delivers productivity at beaumont hospital</a>
+                                </div>
+                            </div>
+                            <div class="single-recent-posts">
+                                <img src="assets/images/news/1.jpg" alt="recent post">
+                                <div class="recent-post-content">
+                                    <p>January 09, 2022</p>
+                                    <a href="single.php">Tips to make your workforce a security front line</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!--single sidebar area end-->
+
+                    <!--single sidebar area start-->
+                    <div class="ezze-single-sidebar">
+                        <div class="ezze-sidebar-header">
+                            <h4>Tags</h4>
+                        </div>
+                        <div class="ezze-sidebar-body">
+                            <ul class="tags-widget">
+                                <li><a href="blog.php">IT Services</a></li>
+                                <li><a href="blog.php">IT Management</a></li>
+                                <li><a href="blog.php">Cyber Security</a></li>
+                                <li><a href="blog.php">Cyber Consultant</a></li>
+                                <li><a href="blog.php">Architecture</a></li>
+                                <li><a href="blog.php">UI/ UX kit</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--single sidebar area end-->
+                </aside>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Single page area end-->
+<?php include 'footer.php';?>
